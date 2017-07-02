31cef0bd7266394a52aba83a0c45dc09c873f537
Ryan Cabanas <ryancabanas@users.noreply.github.com>
2017-07-01 21:34:10 -0400

    backspace test

---
+++ src/experiment/Experiments.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package experiment;
+
+/**
+ *
+ * @author Ryan Cabanas
+ */
+public class Experiments
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Fart\n\bFast");
+  }
+}
