3459f0881b040a10c78b16dbb35cf2e253bea2fb
Ryan Cabanas <ryancabanas@users.noreply.github.com>
2017-07-02 15:45:37 -0400

    Finishing up bitwise operator tests.

--- src/experiment/Experiments.java
+++ src/experiment/Experiments.java
@@ -25,15 +25,29 @@ public class Experiments
     
     //Testing bitwise operators
     int testAND = 8 & 9;
-    System.out.println(Integer.toBinaryString(8) + " & " + Integer.toBinaryString(9)
-            + " = " + Integer.toBinaryString(testAND));
+    System.out.println(toPaddedBinaryString(8, 4)
+            + " & " + toPaddedBinaryString(9, 4)
+            + " = " + toPaddedBinaryString(testAND, 4));
     
     int testOR = 1 | 2 | 4;
-    System.out.println(
-            Integer.toBinaryString(1)
-                    + " | " + Integer.toBinaryString(2)
-                    + " | " + Integer.toBinaryString(4)
-                    + " = " + Integer.toBinaryString(testOR));
+    System.out.println(toPaddedBinaryString(1, 4)
+            + " | " + toPaddedBinaryString(2, 4)
+            + " | " + toPaddedBinaryString(4, 4)
+            + " = " + toPaddedBinaryString(testOR, 4));
+    // The AND (&) can turn bits "off" with a "0" bit mask.  It can also check the "1" status of
+    // another bit with a mask bit of "1".
+    //
+    // The OR (|) can turn bits "on" with a "1" bit mask.
+    //
+    // The NOT (~) flips bits to the opposite of what they were.  There is no mask.
+    //
+    // The XOR (^) is a mix of AND and OR.  "0" + "0" = "0" ... "0" + "1" = "1" ... "1" + "1" = "0".
+    // This, of course, uses a mask.
     
+  }
+  
+  private static String toPaddedBinaryString(int number, int width) {
+    String formatter = "%" + width + "s";
+    return String.format(formatter, Integer.toBinaryString(number)).replace(" ", "0");
   }
 }
