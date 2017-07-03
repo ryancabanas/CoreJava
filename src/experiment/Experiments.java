8fce5bc1567833b953c6289ff39ce4c5ab145b74
Ryan Cabanas <ryancabanas@users.noreply.github.com>
2017-07-02 22:28:20 -0400

    Now I'm really done with the bitwise operators.

--- src/experiment/Experiments.java
+++ src/experiment/Experiments.java
@@ -24,16 +24,60 @@ public class Experiments
     // Didn't know this, but a modulus is calculated as: (x - ((x/y) * y)).
     
     //Testing bitwise operators
-    int testAND = 8 & 9;
-    System.out.println(toPaddedBinaryString(8, 4)
-            + " & " + toPaddedBinaryString(9, 4)
-            + " = " + toPaddedBinaryString(testAND, 4));
+//    int testAND = 8 & 9;
+//    System.out.println(toPaddedBinaryString(8, 4)
+//            + " & " + toPaddedBinaryString(9, 4)
+//            + " = " + toPaddedBinaryString(testAND, 4));
+//    
+//    int testOR = 1 | 2 | 4;
+//    System.out.println(toPaddedBinaryString(1, 4)
+//            + " | " + toPaddedBinaryString(2, 4)
+//            + " | " + toPaddedBinaryString(4, 4)
+//            + " = " + toPaddedBinaryString(testOR, 4));
     
-    int testOR = 1 | 2 | 4;
-    System.out.println(toPaddedBinaryString(1, 4)
-            + " | " + toPaddedBinaryString(2, 4)
-            + " | " + toPaddedBinaryString(4, 4)
-            + " = " + toPaddedBinaryString(testOR, 4));
+//    int testXOR = Integer.parseInt("1010", 2) ^ Integer.parseInt("1111", 2);
+//    System.out.println(toPaddedBinaryString(testXOR, 4));
+//    
+//    int testNOT = ~Integer.parseInt("1110", 2);
+//    System.out.println(toPaddedBinaryString(testNOT, 4));
+//    
+//    int leftShift = Integer.parseInt("0001", 2) << 4;
+//    System.out.println(toPaddedBinaryString(leftShift, 4));
+    
+//    System.out.println(toPaddedBinaryString(8, 32));
+//    int rightShift = 8 >> 2;
+//    System.out.println(toPaddedBinaryString(rightShift, 32));
+//    
+//    System.out.println(toPaddedBinaryString(-8, 32));
+//    int rightShift1 = -8 >> 2;
+//    System.out.println(toPaddedBinaryString(rightShift1, 32));
+//
+//    int rightShift3 = -8 >>> 2;
+//    System.out.println(toPaddedBinaryString(rightShift3, 32));
+//
+//    int rightShift31 = -8 >> 31;
+//    System.out.println(toPaddedBinaryString(rightShift31, 32));
+    
+//    int test = -2147483648 >> 1;
+//    int test2 = -2147483648 >>> 1;
+//    int test3 = 2147483647 >> 1;
+//     int test4 = 2147483647 >>> 1;
+//    System.out.println(toPaddedBinaryString(-2147483648, 32));
+//    System.out.println(toPaddedBinaryString(2147483647, 32));
+//    System.out.println();
+//    System.out.println(toPaddedBinaryString(test, 32));
+//    System.out.println(toPaddedBinaryString(test2, 32));
+//    System.out.println(toPaddedBinaryString(test3, 32));
+//    System.out.println(toPaddedBinaryString(test4, 32));
+    
+//    System.out.println(toPaddedBinaryString(-1, 32));
+//    System.out.println(toPaddedBinaryString(-5, 32));
+//    System.out.println(toPaddedBinaryString(-7, 32));
+//    System.out.println(toPaddedBinaryString(-8, 32));
+//    System.out.println(toPaddedBinaryString(-14, 32));
+//    System.out.println(toPaddedBinaryString(-2147483648, 32));
+//    System.out.println(toPaddedBinaryString(-2147483647, 32));
+    
     // The AND (&) can turn bits "off" with a "0" bit mask.  It can also check the "1" status of
     // another bit with a mask bit of "1".
     //
@@ -43,6 +87,10 @@ public class Experiments
     //
     // The XOR (^) is a mix of AND and OR.  "0" + "0" = "0" ... "0" + "1" = "1" ... "1" + "1" = "0".
     // This, of course, uses a mask.
+    //
+    // "<<" and ">>" just shift bits left, or right.  ">>>" shifts bits right, but zeros are entered
+    // on the left edge for fill in the spaces.  This really only makes a difference if you're
+    // shifting a negative number.
     
   }
   
